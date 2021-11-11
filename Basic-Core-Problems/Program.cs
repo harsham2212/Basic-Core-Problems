b6229d8c6c8ea065c29be9a4c5ca76cb0a8a9080
Harsh Singh <kmrharsh50@gmail.com>
2021-11-11 23:06:55 +0530

    [Harsh]Added:Flip the Coin Basic-Core-Problems Features Implemented

---
+++ Basic-Core-Problems/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Basic_Core_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basic Core Problems /n");
+            Console.WriteLine("Enter the number \n 1. Flip Coin \n 2. Exit");
+            int option = Convert.ToInt32(Console.ReadLine());
+            bool flag = true;
+            while (flag)
+            {
+                switch (option)
+                {
+                    case 1:
+                        FlipCoin coin = new FlipCoin();
+                        coin.Flip();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+
+    }
+}
