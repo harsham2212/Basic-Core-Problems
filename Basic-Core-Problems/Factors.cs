cb07a861aae2a1550a4107a5b9aeef90edb5e06c
Harsh Singh <kmrharsh50@gmail.com>
2021-11-11 23:48:13 +0530

    [Harsh]Added:to calculate Factors Basic-Core-Problems Features Implemented

---
+++ Basic-Core-Problems/Factors.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic_Core_Problems
+{
+    class Factors
+    {
+        public void Calculate()
+        {
+            Console.WriteLine("Enter the Number to Calculate Factor");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i * i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    int count = 0;
+                    while (num % i == 0)
+                    {
+                        num /= i;
+                        count++;
+                    }
+                    Console.WriteLine($"{i}");
+                }
+
+            }
+        }
+    }
+}
--- Basic-Core-Problems/Program.cs
+++ Basic-Core-Problems/Program.cs
@@ -7,7 +7,7 @@ namespace Basic_Core_Problems
         static void Main(string[] args)
         {
             Console.WriteLine("Basic Core Problems /n");
-            Console.WriteLine("Enter the number \n 1. Flip Coin \n 2. Leap Year \n 3.Power of Two \n 4.Harmonic Number \n 5. Exit");
+            Console.WriteLine("Enter the number \n 1. Flip Coin \n 2. Leap Year \n 3.Power of Two \n 4.Harmonic Number \n 5.Factors \n 6. Exit");
             int option = Convert.ToInt32(Console.ReadLine());
             bool flag = true;
             while (flag)
@@ -31,6 +31,10 @@ namespace Basic_Core_Problems
                         sum.Harmonic();
                         break;
                     case 5:
+                        Factors fc = new Factors();
+                        fc.Calculate();
+                        break;
+                    case 6:
                         flag = false;
                         break;
                 }
