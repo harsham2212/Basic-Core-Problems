068d844e0a9f5b369b98dd5bb5b0f7eef53119a9
Harsh Singh <kmrharsh50@gmail.com>
2021-11-12 23:23:44 +0530

    [Harsh]Added:Swapping Two Number Basic-Core-Proble Features Implemented

--- Basic-Core-Problems/Program.cs
+++ Basic-Core-Problems/Program.cs
@@ -7,7 +7,7 @@ namespace Basic_Core_Problems
         static void Main(string[] args)
         {
             Console.WriteLine("Basic Core Problems /n");
-            Console.WriteLine("Enter the number \n 1. Flip Coin \n 2. Leap Year \n 3.Power of Two \n 4.Harmonic Number \n 5.Factors \n 6.Quotient and Remainder \n 7. Exit");
+            Console.WriteLine("Enter the number \n 1. Flip Coin \n 2. Leap Year \n 3.Power of Two \n 4.Harmonic Number \n 5.Factors \n 6.Quotient and Remainder \n 7. Swap Number \n 8. Exit");
             int option = Convert.ToInt32(Console.ReadLine());
             bool flag = true;
             while (flag)
@@ -39,6 +39,10 @@ namespace Basic_Core_Problems
                         val.Calculation();
                         break;
                     case 7:
+                        Swapping ans = new Swapping();
+                        ans.Swap();
+                        break;
+                    case 8:
                         flag = false;
                         break;
                 }
